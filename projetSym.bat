189076565c6ecf712f7da202304db0e44318de5f
Adrien Poignard <poignard.adrien@gmail.com>
2020-12-03 11:39:18 +0100

    changement couleur page suivi de frais

---
+++ projetSym.bat
@@ -0,0 +1,13 @@
+call composer create-project symfony/skeleton:~4.4 Navire
+call cd C:\wamp64\www\Navire
+call composer require symfony/apache-pack
+call composer require symfony/maker-bundle
+call composer require sensio/framework-extra-bundle 
+call composer require symfony/profiler-pack
+call composer require symfony/twig-bundle
+call composer require symfony/asset 
+call Composer require symfony/form			
+call Composer require symfony/validator 
+call composer require symfony/orm-pack
+call composer require symfony/security-bundle
+
