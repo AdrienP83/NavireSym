dd7ab2a74300a3243c08e3b8e90fbe184e80a5c8
Adrien Poignard <adrien.poignard10@gmail.com>
2020-12-08 21:08:11 +0100

    fin partie 3

---
+++ src/Entity/AisShipType.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AisShipTypeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=AisShipTypeRepository::class)
+ */
+class AisShipType
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $aisShiptype;
+
+    /**
+     * @ORM\Column(type="string", length=60)
+     * @Assert\Lenght(min=1,
+     * max=9
+     * minMessage = "Le type d'un navire est compris entre 1et 9",
+     * maxMessage = "Le type d'un navire est compris entre 1 et 9",
+     * allowEmptyString = false
+     * )
+     */
+    private $libelle;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAisShiptype(): ?int
+    {
+        return $this->aisShiptype;
+    }
+
+    public function setAisShiptype(int $aisShiptype): self
+    {
+        $this->aisShiptype = $aisShiptype;
+
+        return $this;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): self
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+}
