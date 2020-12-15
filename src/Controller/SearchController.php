9734d9b1c91120e45e63b2f26054f683fc009664
Adrien Poignard <poignard.adrien@gmail.com>
2020-12-15 12:50:06 +0100

    debut tp 5

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -7,26 +7,43 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+use App\Repository\NavireRepository;
 
-class SearchController extends AbstractController
-{
+class SearchController extends AbstractController {
+
     /**
      * @Route("/search", name="search")
      */
-    public function index(): Response
-    {
+    public function index(): Response {
         return $this->render('search/index.html.twig', [
-            'controller_name' => 'SearchController',
+                    'controller_name' => 'SearchController',
         ]);
     }
-    public function searchBar(){
+
+    public function searchBar() {
         $form = $this->createFormBuilder()
                 ->setAction($this->generateUrl("search_handlesearch"))
                 ->add('cherche', TextType::class)
                 ->add('envoiimo', SubmitType::class)
                 ->add('envoimmsi', SubmitType::class)
                 ->getForm();
-        return $this->render('elements/searchbar.html.twig',['formSearch'=> $form->createView()]);
-                
+        return $this->render('elements/searchbar.html.twig', ['formSearch' => $form->createView()]);
     }
+/**
+ * @Route("/search/handlesearch", name="search_handlesearch")
+ * @param Request $request
+ * @param NavireRepository $repo
+ * @return \App\Controller\Reponse
+ */
+    public function handleSearch(Request $request, NavireRepository $repo): Reponse {
+        $valeur = $request->request->get('form')['cherche'];
+        if (isset($request->request->get('form')['znvoiimo'])) {
+            $critere = "imo Recherché : " . $valeur;
+        } else {
+            $critere = "mmsi recherché : ".$valeur;
+        }
+        return new Response("<h1> $critere </h1>");
+    }
+
 }
